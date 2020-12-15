6121bbf4912fd6aa2d813ca44e9f0c91e1fd3177
Drew Bradley <williamandrewb@gmail.com>
2020-12-14 20:13:34 -0700

    Create query selectors for DOM elements, and begin writing DOM manipulation code

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,69 @@
-console.log("Hello World");
+const User = require("./User");
+const UserRepository = require("./UserRepository");
+const userData = require("../data/users");
+const Hydration = require("./Hydration")
+const hydrationData = require("../data/hydration");
+const Activity = require("./Activity")
+const activityData = require("../data/activity")
+
+const userFirstName = document.querySelector('.user-info__greeting');
+const userInfoDisplay = document.querySelector('.user-info__basic-display');
+
+const hydrationCurrentCount = document.querySelector('.hydration__current');
+const hydrationDate = document.querySelector('.hydration__date');
+const hydrationWeek = document.querySelector('.hydration__week');
+
+const sleepCurrentHours = document.querySelector('.sleep__current-hours');
+const sleepCurrentQuality = document.querySelector('.sleep__current-quality');
+const sleepWeekHours = document.querySelector('.sleep__week-hours');
+const sleepWeekQuality = document.querySelector('.sleep__week-quality');
+const sleepAveragesHours = document.querySelector('.sleep__averages-hours');
+const sleepAverageQuality = document.querySelector('.sleep__averages-quality');
+
+const activityCurrentSteps = document.querySelector('.activity__current-steps');
+const activityCurrentMinutes = document.querySelector('.activity__current-minutes');
+const activityCurrentMiles = document.querySelector('.activity__current-miles');
+const activityWeekSteps = document.querySelector('.activity__week-steps');
+const activityWeekStairs = document.querySelector('.activity__week-stairs');
+const activityWeekMinutes = document.querySelector('.activity__week-minutes');
+
+let allUsers = users.map(userInfo => {
+  const user = new User(userInfo);
+  return user;
+});
+
+let userRepo = new UserRepository(allUsers);
+
+let today;
+let currentUser;
+let activityInfo;
+let hydrationInfo;
+
+const pageLoad = () => {
+  currentUser = userRepo[0];
+  userFirstName.innerText = `Hello! ${currentUser.returnUserFirstName()}!`;
+  showUserInfo();
+};
+
+const showUserInfo = () => {
+  userInfoDisplay.innerHTML = `<li class="user-info__stride-length">Stride: ${currentUser.strideLength}</li>
+  <li class="user-info__friends"> Friends
+    <ul>
+      <li class="friend1">${currentUser.friend[0]}</li>
+      <li class="friend2">${currentUser.friend[1]}</li>
+      <li class="friend3">${currentUser.friend[2]}</li>
+    </ul>
+  </li>
+  <li class="user-info__step-goal">Daily Step Goal: ${currentUser.dailyStepGoal}</li>
+  <li class="user-info__average-step-goal">Average Daily Step Goal: ${userRepo.calculateAverageStepGoal()}</li>`
+};
+
+const showActivityInfo = () => {
+  activityInfo = new Activity(activityData);
+  today = activity.returnLatestDate();
+}
+
+const showHydrationInfo = () => {
+  hydrationInfo = new Hydration(hydrationData);
+  currentUserHydration = hydrationInfo.findDailyWaterIntake(currentUser.id, today)
+}
