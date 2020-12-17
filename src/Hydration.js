1f757f74708f7a68dfaec2794e48d8dcb7fcb13a
Allison Dietz <dietz.allison@outlook.com>
2020-12-17 11:44:32 -0700

    Refactor methods in hydration to get weekly data by a given date

--- src/Hydration.js
+++ src/Hydration.js
@@ -6,16 +6,16 @@ class Hydration {
   calculateAverageWaterIntake(userID) {
     const currentUserHydrationData = [];
     const totalWaterIntake = this.hydrationData.reduce((sum, hydrationInfo) => {
-      if(hydrationInfo.userID === userID) {
+      if (hydrationInfo.userID === userID) {
         currentUserHydrationData.push(hydrationInfo)
         sum += hydrationInfo.numOunces;
-        }
+      }
       return sum;
     }, 0);
     return Math.floor(totalWaterIntake / currentUserHydrationData.length);
   }
 
-  findDataByUser(userID) {
+  filterDataByUser(userID) {
     const currentUserHydrationData = this.hydrationData.filter(hydrationInfo => {
       return hydrationInfo.userID === userID
     });
@@ -29,19 +29,27 @@ class Hydration {
     return dailyData;
   }
 
+  findWeeklyDataByDate(date, dataSet) {
+    const endDate = new Date(date);
+    const startDate = new Date(Number(endDate));
+    startDate.setDate(startDate.getDate() - 6);
+    const weeklyData = dataSet.filter(dataEntry => {
+      const dataEntryDate = new Date(dataEntry.date);
+      return ((dataEntryDate >= startDate) && (dataEntryDate <= endDate))
+    });
+    return weeklyData;
+  }
+
   findDailyWaterIntake(userID, date) {
-    const currentUserData = this.findDataByUser(userID);
+    const currentUserData = this.filterDataByUser(userID);
     const dailyIntake = this.findDataByDate(date, currentUserData);
     return dailyIntake.numOunces;
   }
 
   findWeeklyWaterIntake(userID, date) {
-    const currentUserData = this.findDataByUser(userID);
-    const startDate = currentUserData.find(info => info.date === date);
-    const endIndex = currentUserData.indexOf(startDate);
-    const startIndex = endIndex - 7;
-    const weeklyDates = currentUserData.slice(startIndex, endIndex);
-    const weeklyOunceCounts = weeklyDates.map(info => info.numOunces)
+    const currentUserData = this.filterDataByUser(userID);
+    const weeklyData = this.findWeeklyDataByDate(date, currentUserData);
+    const weeklyOunceCounts = weeklyData.map(info => info.numOunces)
     return weeklyOunceCounts;
   }
 
